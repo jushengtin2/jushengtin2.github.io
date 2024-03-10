e5811c2e9383d7c9581f180a908a622c7fad7b02
侯岳昇 <houyuesheng@houyueshengdebijixingdiannao.local>
2024-03-11 02:08:58 +0800

    0311

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: isProd ? 'export' : undefined,
   reactStrictMode: true,
-  basePath: isProd ? '' : '', // 如果你的网站是在根域名部署，这里应该为空
-  assetPrefix: isProd ? 'https://jushengtin2.github.io/' : '',
 };
 
 export default nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -10,32 +10,42 @@ function Home() {
     <div className={hellocss.body}>
       
       <div className={hellocss.qq}>
-        <div className={hellocss.title}>WELCOME TO MY WEB</div>
+        <div className={hellocss.circle}></div>
+        <div className={hellocss.triangle}></div>
+        <div className={hellocss.title1}>WELCOME</div>
+        <div className={hellocss.title2}>TO</div>
+        <div className={hellocss.title3}>MY WEB</div>
+        <div className={hellocss.red_rectangle}></div>
+        
       </div>
 
       <div className={hellocss.divv}>
 
-        <Link href="/aboutme"className={hellocss.overlap} >
+        <Link href="/aboutme"className={hellocss.overlap1} >
           <Button className={hellocss.button_img}>
-            <img className={hellocss.rectangle} alt="Rectangle" src="assets/lab.jpeg" />
+            <img className={hellocss.rectangle}  src="assets/lab.jpeg" />
             <div className={hellocss.frame}>
-            <div className={hellocss.textwrapper}>About me</div>
+              <div className={hellocss.textwrapper}>About me</div>
             </div>
           </Button>
         </Link>
 
-          <div className={hellocss.overlapgroup}> 
-            <img className={hellocss.rectangle} alt="Rectangle" src="assets/nanyi.jpeg" />
-            <div className={hellocss.frame}>
-              <div className={hellocss.textwrapper}>SKILLS</div>
-            </div> 
+          <div className={hellocss.overlap2}> 
+            <Button className={hellocss.button_img}>
+              <img className={hellocss.rectangle}  src="assets/nanyi.jpeg" />
+              <div className={hellocss.frame}>
+                <div className={hellocss.textwrapper}>SKILLS</div>
+              </div> 
+            </Button>
           </div> 
 
-          <div className={hellocss.overlap2}>
-            <img className={hellocss.rectangle} alt="Rectangle" src="assets/labstudents.jpeg" /> 
-            <div className={hellocss.frame}> 
-                <div className={hellocss.textwrapper}>COLLECTIONS</div> 
-            </div>
+          <div className={hellocss.overlap3}>
+            <Button className={hellocss.button_img}>
+              <img className={hellocss.rectangle}  src="assets/labstudents.jpeg" /> 
+              <div className={hellocss.frame}> 
+                  <div className={hellocss.textwrapper}>COLLECTIONS</div> 
+              </div>
+            </Button>
           </div>
 
       </div>
