6edb9eca4de965c44ff471e299192e2a1927745f
侯岳昇 <houyuesheng@houyueshengdebijixingdiannao.local>
2024-03-11 00:21:53 +0800

    0309-23

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: isProd ? 'export' : undefined,
   reactStrictMode: true,
-  basePath: isProd ? '/jushengtin.github.io' : '',
-  assetPrefix: isProd ? 'https://jushengtin.github.io/jushengtin.github.io/' : '',
+  basePath: isProd ? '/jushengtin2.github.io' : '',
+  assetPrefix: isProd ? 'https://jushengtin2.github.io/jushengtin2.github.io/' : '',
 };
 
 export default nextConfig;
