4565a12a8ca340e8165a2414725d9274393b437a
侯岳昇 <houyuesheng@houyueshengdebijixingdiannao.local>
2024-03-10 20:49:37 +0800

    0309-12

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,49 @@
 // pages/index.js
-export default function Home() {
-    return <div>hi im justin!</div>
-  }
+import hellocss from '../styles/Home.module.css'
+
+import React from "react";
+import Button from '@mui/material/Button';
+import Link from "next/link";
+
+function Home() {
+  return (
+    <div className={hellocss.body}>
+      
+      <div className={hellocss.qq}>
+        <div className={hellocss.title}>WELCOME TO MY WEB</div>
+      </div>
+
+      <div className={hellocss.divv}>
+
+        <Link href="/aboutme"className={hellocss.overlap} >
+          <Button className={hellocss.button_img}>
+            <img className={hellocss.rectangle} alt="Rectangle" src="assets/lab.jpeg" />
+            <div className={hellocss.frame}>
+            <div className={hellocss.textwrapper}>About me</div>
+            </div>
+          </Button>
+        </Link>
+
+          <div className={hellocss.overlapgroup}> 
+            <img className={hellocss.rectangle} alt="Rectangle" src="assets/nanyi.jpeg" />
+            <div className={hellocss.frame}>
+              <div className={hellocss.textwrapper}>SKILLS</div>
+            </div> 
+          </div> 
+
+          <div className={hellocss.overlap2}>
+            <img className={hellocss.rectangle} alt="Rectangle" src="assets/labstudents.jpeg" /> 
+            <div className={hellocss.frame}> 
+                <div className={hellocss.textwrapper}>COLLECTIONS</div> 
+            </div>
+          </div>
+
+      </div>
+
+    </div>
+    
+
+  );
+}
+
+export default Home
