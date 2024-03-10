8eca057edfadb4cc982b9ff327923cffe78f3316
侯岳昇 <houyuesheng@houyueshengdebijixingdiannao.local>
2024-03-10 16:04:51 +0800

    0309-5

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/jushengtin.github.io",
-    output: "next export",  // <=== enables static exports
     reactStrictMode: true,
   };
   
