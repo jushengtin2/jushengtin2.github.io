5aa99a5f46f1743e094d6447fe4edecedf5a9ffa
侯岳昇 <houyuesheng@houyueshengdebijixingdiannao.local>
2024-03-10 23:50:43 +0800

    0309-18

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,6 @@ const nextConfig = {
   output: isProd ? 'export' : undefined,
   reactStrictMode: true,
   basePath: "/jushengtin.github.io",
-  assetPrefix: isProd
-    ? 'https://jushengtin.github.io/jushengtin.github.io/'
-    : undefined
+  assetPrefix: "https://jushengtin.github.io/jushengtin.github.io/"
 }
 export default nextConfig
